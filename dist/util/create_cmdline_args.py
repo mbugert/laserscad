fbbf69e3785cac014c12e122eb43c990d352e48a
Michael Bugert <bugert@ukp.informatik.tu-darmstadt.de>
2017-10-09 08:25:04 +0200

    User files are not modified anymore, instead everything via OpenSCAD command-line args. Dependency detection obsolete. Better readability in Makefile.

--- dist/util/create_cmdline_args.py
+++ dist/util/create_cmdline_args.py
@@ -10,7 +10,7 @@ def create_recursive_ternary_expr(translations):
 		return 'id=="{}"? {} : {}'.format(name, trans, tail)
 
 
-def writeback(pos_path, scad_folder, scad_destination_paths_file):
+def writeback(pos_path, cmdargs_dest_path):
 	'''Writes optimized object positions into a given SCAD file.
 	'''
 
@@ -19,14 +19,10 @@ def writeback(pos_path, scad_folder, scad_destination_paths_file):
 		reader = csv.reader(pos_file)
 		translations = [(name, "[{},{},0]".format(x, y)) for name, x, y in list(reader)]
 
-	_lpart_translation = "\nfunction _lpart_translation(id) = " + create_recursive_ternary_expr(translations)
+	_lpart_translation = "function _lpart_translation(id) = " + create_recursive_ternary_expr(translations)
 
-	with open(scad_destination_paths_file, 'r') as paths_file:
-		for line in paths_file:
-			scad_path = os.path.join(scad_folder, line.strip())
-			with open(scad_path, 'a') as scad_file:
-				scad_file.write(_lpart_translation)
-				scad_file.write("\n_laserscad_mode=2;")
+	with open(cmdargs_dest_path, 'w') as dest:
+		dest.write(_lpart_translation)
 
 if __name__=="__main__":
 	writeback(*sys.argv[1:])
